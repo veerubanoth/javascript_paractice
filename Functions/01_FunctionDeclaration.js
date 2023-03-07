b3b30bba333c76e222535c0a2a41878c47db880d
veerubanoth <veerubanoth@gmail.com>
2023-03-07 22:42:09 +0530

    Function Declaration

---
+++ Functions/01_FunctionDeclaration.js
@@ -0,0 +1,18 @@
+// A function is a block of code that performs a specific task.
+// A function encapsulates multiple statements, expressions, and logic.
+// Functions are reusable and remove redundancy by allowing you to encapsulate your code logic for reuse.
+// Technically Functions are classified as objects in javascript.
+
+// Parts of Function Declaration
+//===============================
+//1. Function Keyword
+//2. Function Name
+//3. Parameters
+//4. Code Block
+//5. Return Keyword
+
+function functionName(parameter1, parameter2) {
+  return parameter1 + parameter2;
+}
+//Calling Function
+functionName(25, 20);
