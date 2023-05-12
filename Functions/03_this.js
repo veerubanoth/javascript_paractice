289223ffae1b5ea153eca204dd80cc546336a5ea
VEERU <71265815+veerubanoth@users.noreply.github.com>
2023-05-12 15:38:29 +0530

    new macbook air

--- Functions/03_this.js
+++ Functions/03_this.js
@@ -3,19 +3,19 @@
 //~! THIS in Global Scope
 // console.log(this);
 gfn = function () {
-   this.count = 1;
-   console.log(this.count);
-}
-
-function lfn() {
-   this.count++;
-   console.log(this.count);//~> Outer functioins, 'this' points to global ('this' in functions outside objects)
+  this.count = 1;
+  console.log(this.count);
 };
 
-var vfn = function () {
-   this.count += 2;
-   console.log(this.count);
+function lfn() {
+  this.count++;
+  console.log(this.count); //~> Outer functioins, 'this' points to global ('this' in functions outside objects)
 }
+
+var vfn = function () {
+  this.count += 2;
+  console.log(this.count);
+};
 
 gfn();
 lfn();
@@ -25,37 +25,32 @@ vfn();
 //~% The 'this' keyword points to the object allowing us to access
 //~% - object's properties & values when the function is part of an object.
 let user = {
-   name: 'Jhon',
-   age: 30,
-   sayHi: function () {
-      console.log(this.name); //* The value of this will be user
-   }
-}
+  name: "Jhon",
+  age: 30,
+  sayHi: function () {
+    console.log(this.name); //* The value of this will be user
+  },
+};
 user.sayHi();
 //~> Technically possible to access properties of an Object
 console.log(user.age);
 
-
 gfn = function () {
-   console.log(`gfn ${this}`)
-}
+  console.log(`gfn ${this}`);
+};
 var vfn = function () {
-   console.log(`vfn ${this}`)
-
-}
+  console.log(`vfn ${this}`);
+};
 function vfn() {
-   console.log(`lfn ${this}`)
+  console.log(`lfn ${this}`);
 }
 
 var person = {
-   name: 'veeru',
-   age: 36,
-   print: function () { }
-}
-
-
+  name: "veeru",
+  age: 36,
+  print: function () {},
+};
 
 //~! THIS inside Constructor functions
 //~! THIS inside callback functions
 //~! THIS inside arrow functions
-
