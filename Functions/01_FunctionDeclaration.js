fd461f6c436e1c11477644dbe9513a7928b348d9
veerubanoth <veerubanoth@gmail.com>
2023-05-19 12:45:59 +0530

    function declaration

--- Functions/01_FunctionDeclaration.js
+++ Functions/01_FunctionDeclaration.js
@@ -1,9 +1,9 @@
-// A function is a block of organized, reusable code that performs a specific task.
-// A function encapsulates multiple statements, expressions, and logic.
-// Functions are reusable and remove redundancy by allowing you to encapsulate your code logic for reuse.
-// Technically Functions are classified as objects in javascript.
+//~^ A function is a block of organized, reusable code that performs a specific task.
+//~^ A function encapsulates multiple statements, expressions, and logic.
+//~^ Functions are reusable and remove redundancy by allowing you to encapsulate your code logic for reuse.
+//~^ Technically Functions are classified as objects in javascript.
 
-// Parts of Function Declaration
+//~# Parts of Function Declaration
 //===============================
 //1. Function Keyword
 //2. Function Name
@@ -16,3 +16,24 @@ function functionName(parameter1, parameter2) {
 }
 //Calling Function
 functionName(25, 20);
+
+
+//~! Example 
+function computeRectStats(length, width) {
+  let areaRectA = Number(length) * Number(width);
+  let perimeterRect = 2 * (Number(length)) + Number(length);
+  console.log(`Area: ${areaRectA}`);
+  console.log(`Perimeter: ${perimeterRect}`);
+}
+
+let rectALength = 25;
+let rectAWidth = 10;
+computeRectStats(rectALength, rectAWidth);
+
+let rectBLength = 25;
+let rectBWidth = 10;
+computeRectStats(rectBLength, rectBWidth);
+
+let rectCLength = 25;
+let rectCWidth = 10;
+computeRectStats(rectCLength, rectCWidth);
---
+++ Functions/05_FactoryFunctions.js
@@ -0,0 +1,4 @@
+//~% It's a design pattern that we use in order to reduce code duplication
+//~% and create objects for us efficiantly. 
+
+//~! It's a function that creates objects and returns them.  
